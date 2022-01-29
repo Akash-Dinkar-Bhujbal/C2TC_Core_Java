92ac07d5ebe8b865a5445571df48b985c8015c29
Akash-Dinkar-Bhujbal <akashbhujbal97@gmail.com>
2022-01-29 06:10:32 +0530

    First Commit

---
+++ C2TC_Core_Java/src/day02/JVM_Notes.java
@@ -0,0 +1,12 @@
+package day02;
+
+public class JVM_Notes {
+/*
+ * Java Development Kit: Development + Running (java program)
+ * Java Runtime Environment : Running for java program
+ * Java Virtual Machine : JVM is a s/w simulation of m/c which operates exactly like physical m/c
+ * 
+ * 
+ * 
+ * */
+}
